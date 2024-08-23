c654c66e89fc48e44fc59835d9be51ff7d9bbf41
GustavoeDev <gustavoe.dev@gmail.com>
2024-08-22 23:19:04 -0300

    cards em js atualizados

--- js/scripts.js
+++ js/scripts.js
@@ -1,10 +1,41 @@
-// Carrossel Cards
+document.addEventListener("DOMContentLoaded", () => {
 
+const activateCard = (cardToActivate) => {
+        
+const cardPadrao = document.querySelector("#card-img");
+cardPadrao.classList.remove("active");
+   
+document.querySelectorAll(".details-card").forEach(card => {
+    card.classList.remove("active");
+});
+     
+cardToActivate.classList.add("active");
+};
+
+    
+const detailsJs = document.querySelector(".details-js");
+
+detailsJs.addEventListener("click", (event) => {
+        
+    if (event.target.matches(".img-cod, .img-valorant, .img-pubg, .img-lol, .img-fifa")) {
+        const imgClass = event.target.classList[0]; 
+        const cardId = imgClass.replace('img-', 'card-'); 
+        const cardToActivate = document.querySelector(`#${cardId}`);
+        if (cardToActivate) {
+            activateCard(cardToActivate);
+        }
+    }
+});
+
+   
 const copy = document.querySelector(".card");
 const clone = copy.cloneNode(true);
 const details = document.querySelector(".details-js");
-
-
 details.appendChild(clone);
 
-
+// Verificação de Classes
+const clonedImages = details.querySelectorAll('.card img');
+clonedImages.forEach(img => {
+    img.classList.add(img.src.split('/').pop().split('.')[0]); // Adiciona a classe apropriada
+});
+});
